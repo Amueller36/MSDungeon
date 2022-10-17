8413fb8f83a1ff7dcc7d350a02896f8aa20f443b
Genetics <102921542+Amueller36@users.noreply.github.com>
2022-10-17 17:10:09 +0200

    Initial commit

---
+++ src/test/kotlin/com/example/msdungeon/MsDungeonApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.msdungeon
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MsDungeonApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
